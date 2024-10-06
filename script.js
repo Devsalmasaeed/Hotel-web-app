9a992c535fba5da03f1637312e3c8a28f0b2321d
iam <sasaeed092@gmail.com>
2024-10-06 12:04:11 +0100

    Added card js file

--- script.js
+++ script.js
@@ -51,3 +51,52 @@ const renderProduct = chooseusContainer => {
 }
 
 renderProduct(chooseusContainer);
+
+
+const specialofferContainer = [
+    { 
+        Image: "assets/Special Offers img1.png", 
+        img: "assets/rating.svg",
+        img2: "assets/favorite.svg",
+        heading1: "Wilderness Club at Big <br> Ceddar",
+        heading2: "28 October — 1 November",
+        para: "$2016<span>/6 night</span>",
+    },
+    { 
+        Image: "assets/Special Offers img2.png", 
+        img: "assets/rating.svg",
+        img2: "assets/favorite.svg",
+        heading1: "Wilderness Club at Big <br> Ceddar",
+        heading2: "28 October — 1 November",
+        para: "$2016<span>/6 night</span>",
+    },
+    { 
+        Image: "assets/Special Offers img3.png", 
+        img: "assets/rating.svg",
+        img2: "assets/favorite.svg",
+        heading1: "Wilderness Club at Big <br> Ceddar",
+        heading2: "28 October — 1 November",
+        para: "$2016<span>/6 night</span>",
+    },
+
+];
+
+const renderContainer = specialofferContainer => {
+    const container = document.getElementById('specialofferContainer');
+        specialofferContainer.forEach(specialofferContainer => {
+        container.innerHTML += `
+            <div class="card">
+                <img src="${specialofferContainer.Image}" alt="" id="bed">
+                <div class="img">
+                    <img src="${specialofferContainer.img}" alt="">
+                    <img src="${specialofferContainer.img2}" alt="" id="fav">
+                </div>
+                <h2>${specialofferContainer.heading1}</h2>
+                <h3>${specialofferContainer.heading2}</h3>
+                <p>${specialofferContainer.para}</p>
+            </div>
+        `;
+    });
+}
+
+renderContainer(specialofferContainer);
